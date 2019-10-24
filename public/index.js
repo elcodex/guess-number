897e07413d3cafe33f7ca2b45ca0657635e00ef7
Elen <elcrses@ya.ru>
2019-10-24 17:23:56 +0300

    send user form to server

--- public/index.js
+++ public/index.js
@@ -1,3 +1,21 @@
+document.querySelector('.form__button').addEventListener('click', e => {
+    const data = new FormData(document.querySelector('.form'));
+    
+    displayMessage(data.get('message'), 'you');   
+    e.preventDefault();
+   
+    let xmlHttpRequest = new XMLHttpRequest();
+    xmlHttpRequest.onerror = _ => console.log('error', xmlHttpRequest.statusText);
+    xmlHttpRequest.onloadstart = _ => console.log('load starts');
+    xmlHttpRequest.onloadend = _ => console.log('load ends', xmlHttpRequest.statusText);
+    xmlHttpRequest.onload = _ => console.log('load', xmlHttpRequest.statusText);
+
+    xmlHttpRequest.open('post', '/');
+    xmlHttpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xmlHttpRequest.send(`message=${data.get('message')}`);
+
+});
+
 let displayMessage = (text, from) => {
     let messageContainer = document.createElement('div');
     messageContainer.classList.add(`${from}-message`);
