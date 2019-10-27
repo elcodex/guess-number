324141b553c443a1cf134e85ae1447876e421af7
Elen <elcrses@ya.ru>
2019-10-27 12:36:10 +0300

    improve parseRequest

--- scripts/talk.js
+++ scripts/talk.js
@@ -4,14 +4,15 @@ commands: hello , start , end , [number]
  */
 const COMMANDS = ['hello', 'start', 'end'];
 const DO_NOTHING = 'do nothing';
+const NUMBER = 'number';
 
 let parseRequest = request => {
-    const text = request.message.split(' ', 1)[0].toLowerCase();
+    const text = request.body.message.split(/ |\n/, 1)[0].toLowerCase();
     const n = Number(text);
     if (COMMANDS.includes(text))
         return {command: text};
     if (!isNaN(n))
-        return {number: n}
+        return {command: NUMBER, number: n}
     return {command: DO_NOTHING}        
 }
 
