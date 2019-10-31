66a6c40420a35857ac3d76d1fdcba4b0d8e0ea71
Elen <elcrses@ya.ru>
2019-10-31 23:06:49 +0300

    move client data to store.js

---
+++ scripts/store.js
@@ -0,0 +1,33 @@
+let store = new Map();
+
+exports.getInfo = (sessionId, firstVisit) => {
+    if (!store.has(sessionId)) {
+       store.set(sessionId, {number: -1}); 
+    }
+    return store.get(sessionId);
+}
+exports.remove = (sessionId, firstVisit) => {
+    store.remove(sessionId);
+}
+exports.addParam = (sessionId, {key, value}) => {
+    let params = {};
+    if (store.has(sessionId)) {
+        params = store.get(sessionId);
+    }
+    params[key] = value;
+    store.set(sessionId, params);
+}
+exports.addKey = (sessionId) => {
+    if (!store.has(sessionId)) {
+        store.set(sessionId, {number: -1});
+    }
+}
+exports.getParam = (sessionId, param) => {
+    if (store.has(sessionId)) {
+        return store.get(sessionId)[param];
+    }
+    return undefined;
+}
+exports.clearKey = (sessionId) => {
+    store.set(sessionId, {number: -1});
+}
