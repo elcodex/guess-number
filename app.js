41964fdfa80769fee6371a0489916584ec67e3a6
Elen <elcrses@ya.ru>
2019-10-31 23:04:12 +0300

    add sessionId

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const talk = require('./scripts/talk.js');
 const express = require('express');
 const consolidate = require('consolidate');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+
 const app = express();
 const port = 3001;
 
@@ -12,12 +14,19 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(session({
+    //cookie: {secure: true},
+    secret: 'ytu iujhimnbhu986 5rwes<DSWSFRCC FRC',
+    resave: false,
+    saveUninitialized: false,
+}));
 
 //app.get('/', (req, res) => res.send('Hello from server!'));
 app.get('/', (req, res) => res.render('index.html'));
 
 app.post('/', function(req, res) {
-    console.log('request',req.body);
+    if (!req.session.userId) req.session.userId = req.session.id;
+    console.log('sessionId', req.session.userId, req.sessionID);
     const answer = talk.serverAnswer(req);
     console.log(req.body.message, answer);
     res.send(answer);
--- scripts/talk.js
+++ scripts/talk.js
@@ -1,7 +1,6 @@
-/*
-commands: hello , start , end , [number]
+const game = require('./game.js');
+const appStore = require('./store.js');
 
- */
 const COMMANDS = ['hello', 'start', 'end'];
 const DO_NOTHING = 'do nothing';
 const NUMBER = 'number';
@@ -16,70 +15,53 @@ let parseRequest = request => {
     return {command: DO_NOTHING}        
 }
 
-//guess the number
-let game = (from, to) => {
-    let number = from; //from + Math.floor(Math.random() * (to - from));
-    let triesCount = 0;
-    const ANSWERS = {TOO_SMALL: 'too small', TOO_BIG: 'too big', RIGHT: 'right'};
-
-    let answer = userNumber => {
-       // console.log('number', number, userNumber);
-        triesCount++;
-        if (userNumber < number)
-            return ANSWERS.TOO_SMALL;
-        if (userNumber > number)
-            return ANSWERS.TOO_BIG;
-        return ANSWERS.RIGHT;    
-    }
-    let start = _ => {
-        number = from + Math.floor(Math.random() * (to - from));
-        triesCount = 0;
-       // console.log('start number:', number);
-    }
-    let end = _ => {
-        return {number, triesCount}
-    }
-    let allAnswers = _ => ANSWERS;
-    return {
-        answer,
-        start,
-        end,
-        allAnswers,
-    }
-}
-let myGame = game(1, 100);
-
 exports.serverAnswer = request => {
     let action = parseRequest(request);
     let answer = '';
+    const sessionId = request.session.userId;
     if ('command' in action) {
         if (action.command === 'hello') {  //hello
             answer = "Hello! Let's play a game. I think of the number from 1 to 100 and you try to guess it.";
-            answer += "\nTo start the game type 'start'";  
+            answer += "\nTo start the game type 'start'";
+            appStore.addKey(sessionId);
         }
         else if (action.command === 'start') {
-            myGame.start();
+            const number = game.start(1, 100);
+            appStore.addParam(sessionId, {key: 'number', value: number});
+            appStore.addParam(sessionId, {key: 'tries', value: 0});
             answer = "Great! Now you type your guess and i will give you some hints";
         }
         else if (action.command === 'number') {
-            const gameAnswer = myGame.answer(action.number);
-            console.log(action.number, gameAnswer);
-            if (gameAnswer === myGame.allAnswers().TOO_SMALL) {
+            const tries = appStore.getParam(sessionId, 'tries');
+            const guessNumber = appStore.getParam(sessionId, 'number');
+            const gameAnswer = game.turn(action.number, guessNumber, tries);
+            appStore.addParam(sessionId, {key: 'tries', value: gameAnswer.tries});
+            console.log(action.number, guessNumber, tries, gameAnswer);
+            if (gameAnswer.answer === game.allAnswers().TOO_SMALL) {
                 answer = 'Yours number is smaller than my number.';
-            } else if (gameAnswer === myGame.allAnswers().TOO_BIG) {
+            } else if (gameAnswer.answer === game.allAnswers().TOO_BIG) {
                 answer = 'Your guess is too big. Try another number.'
-            } else if (gameAnswer === myGame.allAnswers().RIGHT) {
-                const gameResults = myGame.end();
+            } else if (gameAnswer.answer === game.allAnswers().RIGHT) {
+                const gameResults = {
+                    number: appStore.getParam(sessionId, 'number'),
+                    tries: appStore.getParam(sessionId, 'tries'),
+                }
+                appStore.clearKey(sessionId);
                 answer = 'You got it! Congrats!';
-                answer += '\nTries: ' + gameResults.triesCount + '. Number: ' + gameResults.number;
+                answer += '\nTries: ' + gameResults.tries + '. Number: ' + gameResults.number;
             }
         }
         else if (action.command === 'end') {
-            const gameResults = myGame.end();
+            const gameResults = {
+                number: appStore.getParam(sessionId, 'number'),
+                tries: appStore.getParam(sessionId, 'tries'),
+            }
+            appStore.clearKey(sessionId);
             answer = 'Ok. The number was ' + gameResults.number;
             answer += "\nLet's try again.";
         }
     }
+    console.log(appStore.getInfo(sessionId));
     return answer; 
 }
 
